0089aa063812493562e98fca6ca81c242ab0ae4c
Mr Wait <brennanmitchell1192@gmail.com>
2023-10-01 14:20:16 -0500

    Major adjustments to functions takeTest, checkCorrect.

--- script.js
+++ script.js
@@ -10,41 +10,40 @@
 
 var timerElement = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button");
+var remove = document.querySelector("#startRemove")
+var userResponse = document.querySelector("#possibleAnswers")
 var timer;
 var timerCount;
 var score = 0;
+var qCounter = 0;
+
 
 // var answer = questions[answers[" "]];
 
 // Questions for test
-var questions = [ 
+var questions = [
   {
-    question1: "What is a value that can change depending on conditions or on information passed to the program?",
+    question: "What is a value that can change depending on conditions or on information passed to the program?",
     answers: [
-    {text: "Variable", isCorrect: true }, 
-    {text: "Boolean", isCorrect: false }, 
-    {text: "String", isCorrect: false }, 
-    {text: "Constant", isCorrect: false }
-  ]
+      "Variable", "Array", "String", "Constant"
+    ],
+    correctAnswer: "Variable"
   },
 
   {
- question2: "What can we use to store a list of multiple data types?",
+    question: "What can we use to store a list of multiple data types?",
     answers: [
-    {text: "Variable", isCorrect: false }, 
-    {text: "Array", isCorrect: true },
-    {text: "String", isCorrect: false },
-    {text: "Constant", isCorrect: false },
-  ]
+      "Variable", "Array", "String", "Constant"
+    ],
+    correctAnswer: "Array"
   },
 
-  { question3: "What is the opposite of a variable?",
+  {
+    question: "What is the opposite of a variable?",
     answers: [
-      {text: "Variable", isCorrect: false },
-      {text: "Array", isCorrect: false }, 
-      {text: "String", isCorrect: false }, 
-      {text: "Constant", isCorrect: true }
-    ]
+      "Variable", "Array", "String", "Constant"
+    ],
+    correctAnswer: "Constant"
   },
 ]
 
@@ -59,10 +58,11 @@ function init() {
 
 // start timer
 function startGame() {
-  timerCount = 60;
+  timerCount = 5;
   startButton.disabled = true;
   startTimer()
   takeTest()
+  // startButton.addEventListener("click", startButton.style.display = "none");
 }
 
 
@@ -70,44 +70,52 @@ function startGame() {
 
 // timer function
 function startTimer() {
-  timer = setInterval(function() {
+  timer = setInterval(function () {
     timerCount--;
     timerElement.textContent = timerCount;
-    if (timerCount >= 0) {
-    if (score && timerCount > 0) {
+    if (timerCount > 0) {
+      if (score && timerCount > 0) {
         clearInterval(timer);
-            }
-            // When all questions are answered or Time=0-> Game over
+      }
+      // When all questions are answered or Time=0-> Game over
     } else if (timerCount === 0) {
       clearInterval(timer);
     }
   }, 1000);
-  console.log(timer)
 }
 
 
 
-  function takeTest() {
-    if (timerCount !== 60) {
-    for (var i=0; i <questions.length; i++) {
-      var userResponse = document.querySelector("#selections")
-      if(userResponse === questions.answers[i])
-      {
-        score++;
-        alert("Correct!");
-      } else {
-        alert("Incorrect!")
-        (timerCount - 10);
-    }
+function takeTest() {
+
+  var quizQuestionsEl = document.querySelector("#quizQuestions")
+  quizQuestionsEl.innerHTML = ""
+  userResponse.innerHTML = ""
+    for (var i = 0; i < questions[qCounter].answers.length; i++) {
+      var answer = questions[qCounter].answers[i]
+      var button = document.createElement("button")
+      button.textContent = answer
+      userResponse.append(button)
 }
-}
+    var p = document.createElement("p")
+    p.textContent = questions[qCounter].question
+    quizQuestionsEl.append(p)
 }
 
 
-// checkCorrect() {
-//   if answers
-// }
+function checkCorrect(event) {
+var userAnswer = event.target.textContent
+var correctAnswer = questions[qCounter].correctAnswer
+if (userAnswer === correctAnswer){
+  console.log("correct")
+} else {
+  console.log("false")
+}
+qCounter++
+takeTest()
+}
 
 
 // Click start button-game begins
 startButton.addEventListener("click", startGame)
+userResponse.addEventListener("click", checkCorrect)
